bc54e80d83380813f7a8f9ca289ac4275f898dd9
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-14 20:25:40 +0300

    imported two icon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Navbar.css'
 import menu_icon from '../../assets/menu.png'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
     <nav className='flex-div'>
       <div className='nav-left flex-div'>
         <img src={menu_icon} alt="" />
+        <img src={logo} alt="" />
       </div>
     </nav>
   )
