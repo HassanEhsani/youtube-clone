107698fbc5769b3549a15dcd1b64bf256dc8501c
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-17 18:03:43 +0300

    mount feed components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,15 @@
 import React from 'react'
 import './Home.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Feed from '../../Components/Feed/Feed'
 
 const Home = ({sidebar}) => {
   return (
     <>
       <Sidebar sidebar={sidebar}/>
+      <div className={`container ${sidebar?"":'larg-container'}`}>
+        <Feed/>
+      </div>
     </>
   )
 }
