8055aedf508e6aa91696ad0882ddd127b3ea9a29
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-17 20:53:53 +0300

    wrote the code in the playvideo

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './Video.css'
+import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 
 const Video = () => {
   return (
     <div className='play-container'>
-      
+      <PlayVideo/>
     </div>
   )
 }
