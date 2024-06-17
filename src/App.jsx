278bd72d0b4a8c5f95bcd419fe00dfd9d48fc2be
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-17 13:53:37 +0300

    wrote the route for Home and Video

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
+      </Routes>
     </div>
   )
 }
