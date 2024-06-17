60ae8dd68a90d3ebf0c04d6d9eb555d98174d096
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-17 17:56:16 +0300

    wrote the code about the sidebar when click on it

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -14,11 +14,11 @@ import simon from '../../assets/simon.png'
 import tom from '../../assets/tom.png'
 import megan from '../../assets/megan.png'
 import cameron from '../../assets/cameron.png'
+// import { shape, string } from 'prop-types';
 
-
-const Sidebar = () => {
+const Sidebar = ({sidebar}) => {
   return (
-    <div className='sidebar'>
+    <div className={`sidebar ${sidebar?"":"small-sidebar"}`}>
       <div className='sortcut-links'>
         <div className='side-link'>
           <img src={home} alt="" /><p>Home</p>
